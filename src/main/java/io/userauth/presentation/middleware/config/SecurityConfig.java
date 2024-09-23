0fac616d3b4d761e498aed7cacb98fea4010d173
unknown <naufalhmuzakki@gmail.com>
2024-09-23 22:01:52 +0700

    securityConfig bugfix it worked finally

--- src/main/java/io/userauth/presentation/middleware/config/SecurityConfig.java
+++ src/main/java/io/userauth/presentation/middleware/config/SecurityConfig.java
@@ -1,29 +1,29 @@
 package io.userauth.presentation.middleware.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//             .csrf(csrf -> csrf.disable()) 
-//             .authorizeHttpRequests(auth -> auth
-//                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
-//                 .requestMatchers("/users/public/register").permitAll() 
-//                 .anyRequest().authenticated() 
-//             ).httpBasic();
-//             // .addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
-//             return http.build();
-//     }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) 
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .requestMatchers("/users/public/register").permitAll() 
+                .anyRequest().authenticated() 
+            ).httpBasic();
+            // .addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+            return http.build();
+    }
 
 
-// }
+}
 
