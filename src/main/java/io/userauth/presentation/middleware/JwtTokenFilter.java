072f35ad737d3e7fd274c27f023a914cfd4e13cd
unknown <naufalhmuzakki@gmail.com>
2024-09-25 09:59:23 +0700

    extractAllClaim function f for jwtValue from cookies

--- src/main/java/io/userauth/presentation/middleware/JwtTokenFilter.java
+++ src/main/java/io/userauth/presentation/middleware/JwtTokenFilter.java
@@ -1,12 +1,18 @@
 package io.userauth.presentation.middleware;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import io.userauth.util.CookieUtils;
-import io.userauth.util.JWTUTtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +21,10 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtTokenFilter extends OncePerRequestFilter {
   
+    @Value("${jwt.secret}")
+    String secretKey;
+
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -28,12 +38,27 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         String jwtToken = CookieUtils.getCookieValue(request, "token").getValue();
 
-        if (jwtToken == null || !JWTUTtils.validateToken(jwtToken)) {
+        if (jwtToken == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Invalid or missing JWT token");
+            response.getWriter().write("missing JWT token");
             return;
         }
 
-        filterChain.doFilter(request, response);
+        try {
+            Claims claim = extractALlClaims(jwtToken);
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("invalid Token");
+        }      
+    }
+    
+    private Claims extractALlClaims(String token){
+        SecretKey Key = Keys.hmacShaKeyFor(this.secretKey.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder()
+            .setSigningKey(Key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
